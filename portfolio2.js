4ecd06899b53cd483b4a9cbc62c6f05a40be9be9
SharanyaS-hetty <sharanya_shetty@thbs.com>
2021-12-07 15:49:43 +0530

    first commit

---
+++ portfolio2.js
@@ -0,0 +1,36 @@
+const http = require('http');
+const  fs = require('fs');
+
+
+http.createServer((req,res) => {
+    res.writeHead(200,{'content-type':'text/html'});
+    if(req.url === '/index' || req.url ==='/')
+    {
+        let data = fs.readFileSync('static/index.html');
+        res.write(data);
+    }
+   else if(req.url === '/about')
+    {
+        let data = fs.readFileSync('static/about.html');
+        res.write(data);
+    }
+    else if(req.url === '/projects')
+    {
+        let data = fs.readFileSync('static/projects.html');
+        res.write(data);
+    }
+    else if(req.url === '/social-media')
+    {
+        let data = fs.readFileSync('static/social-media.html');
+        res.write(data);
+    }
+    else{
+        let data= fs.readFileSync('static/error.html');
+        res.write(data);
+    }
+
+ 
+  
+    res.end();
+        
+    }).listen(8082, () => {console.log('server started at port 8080')});
